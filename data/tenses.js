bcf647b7332e78fdb020a66a7a4b90fc9c75cbb9
HM21-88 <scrubit@duck.com>
2026-07-14 00:06:29 +0200

    Bug: tenses.js wordt niet geladen

--- data/tenses.js
+++ data/tenses.js
@@ -1,10 +1,12 @@
+alert('TENSES FILE');
+
 window.tenseExercises = [
   {
-    level:'makkelijk',
-    question:'Test ___ vraag.',
-    translation:'Test vraag.',
-    choices:['ta','a','lo'],
-    answer:'ta',
-    explanation:'Test'
+    level: 'makkelijk',
+    question: 'Test',
+    translation: 'Test',
+    choices: ['ta'],
+    answer: 'ta',
+    explanation: 'Test'
   }
 ];
