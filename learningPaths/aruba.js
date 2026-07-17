82c0644a65ece458dea00c84aed7f4f40452af23
HM21-88 <scrubit@duck.com>
2026-07-17 20:02:10 +0200

    New structure

---
+++ learningPaths/aruba.js
@@ -0,0 +1,39 @@
+window.learningPaths = {
+
+  aruba: {
+
+    title:
+      'One Happy Island',
+
+    guide:
+      'Ana',
+
+    stops: [
+
+      {
+        id:'airport',
+        title:
+          'Reina Beatrix Airport',
+        icon:'plane',
+        words:[]
+      },
+
+      {
+        id:'taxi',
+        title:'Taxi',
+        icon:'car',
+        words:[]
+      },
+
+      {
+        id:'hotel',
+        title:'Hotel',
+        icon:'building',
+        words:[]
+      }
+
+    ]
+
+  }
+
+};
